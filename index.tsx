e6606dd3207aaca01a0e8cbbf19a75e959c8cec4
Jason Wang <imings@icloud.com>
2022-06-18 19:16:25 +0800

    update dir

--- index.tsx
+++
@@ -1,6 +0,0 @@
-import React = require('react');
-import ReactDOM = require('react-dom');
-import App = require('./App');
-import './style.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
